031a7127ce4c49b69b0da17bcf56f383780f6d66
Hannes Lohmander <hannes@lohmander.me>
2016-11-20 22:59:02 +0100

    Add some more fields

--- brink/fields.py
+++ brink/fields.py
@@ -1,3 +1,5 @@
+import sys
+
 class FieldError(Exception): pass
 class FieldRequired(FieldError): pass
 
@@ -15,7 +17,43 @@ class Field(object):
             raise FieldRequired()
         return data
 
-class PasswordField(Field):
+class FieldInvalidType(FieldError): pass
+
+class IntegerField(Field):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def validate(self, data):
+        if type(data) is not int:
+            raise FieldInvalidType()
+        return super().validate(data)
+
+class FieldInsufficientLength(FieldError): pass
+
+class CharField(Field):
+    def __init__(self, *args, min_length=0, max_length=sys.maxsize, **kwargs):
+        self.min_length = min_length
+        self.max_length = max_length
+        super().__init__(*args, **kwargs)
+
+    def validate(self, data):
+        if len(data) < self.min_length:
+            raise FieldInsufficientLength()
+
+        if len(data) > self.max_length:
+            raise FieldInsufficientLength()
+
+        return super().validate(data)
+
+class PasswordField(CharField):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **{"hidden": True, **kwargs})
 
+class BooleanField(Field):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def validate(self, data):
+        if type(data) is not bool:
+            raise FieldInvalidType()
+        return super().validate(data)
